c0ee692bd1c742ae30f2dddfbcdda00de8f5624c
lauanderson-rael <lauanderson38@gmail.com>
2024-11-28 01:00:31 -0300

    adicionado lista04 de ED, incompleta ainda

--- ESTRUTURA DE DADOS/EDExercicios/src/ifma/lista03/TestListaSimples.java
+++ ESTRUTURA DE DADOS/EDExercicios/src/ifma/lista03/TestListaSimples.java
@@ -22,7 +22,6 @@ public class TestListaSimples {
        
     }
         
-        
 
 }
 
---
+++ ESTRUTURA DE DADOS/EDExercicios/src/ifma/lista04/Node.java
@@ -0,0 +1,12 @@
+package ifma.lista04;
+
+public class Node {
+    int element;
+    Node next;
+
+    public Node(int element){
+        this.element = element;
+    }
+}
+
+
