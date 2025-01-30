908a7026c372d5da3d6f62d1fd3dd95da9171e6b
Lauanderson Rael Costa Gomes <20232ADS.CNT0002@ifma.edu>
2025-01-30 20:44:28 -0300

    update novas questoes

---
+++ PROGRAMACAO O_OBJETOS/exercicios/src/ex3_Calculadora/Calculadora.java
@@ -0,0 +1,12 @@
+package ex3_Calculadora;
+
+public class Calculadora {
+
+    public double Somar (double n1, double n2){
+        return n1+n2;
+    }
+    public double Sub(double n1, double n2){
+        return n1 - n2;
+    }
+
+}
